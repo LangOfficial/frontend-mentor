f13aa916212d75db3ad85036043d21c8ae120fae
LangOfficial <119911182+LangOfficial@users.noreply.github.com>
2024-03-17 16:40:41 -0700

    Add files via upload

--- advice-gen-app/public/build/script.js
+++ advice-gen-app/public/build/script.js
@@ -1,10 +1,10 @@
 const patternDivider = document.getElementById("pattern-divider");
-const adviceSlipUrl = "	https://api.adviceslip.com/advice";
 const advice = document.getElementById("advice");
 const adviceNum = document.getElementById("advice-number");
 const adviceAnimation = Array.from(document.querySelectorAll(".advice-animation"));
 const dice = document.getElementById("dice");
-
+let adviceText = '';
+let adviceId = '';
 window.addEventListener('resize', () => {
   if (window.innerWidth >= 1440) {
     patternDivider.src = "build/images/pattern-divider-desktop.svg";
@@ -14,29 +14,40 @@ window.addEventListener('resize', () => {
   }
 })
 
-dice.addEventListener('click', () => {
-  fetch(adviceSlipUrl)
-  .then(response => {
+async function fetchRandomAdvice() {
+  try {
+    adviceAnimation.forEach(adviceEle => {
+      adviceEle.classList.remove('animate-fadeOut');
+      adviceEle.classList.remove('animate-fadeIn');
+    });
+    const randomId = Math.floor(Math.random() * 224) + 1;
+  
+
+    const response = await fetch(`https://api.adviceslip.com/advice/${randomId}`);
     if (!response.ok) {
-      throw new Error('Network response was not ok');
+      throw new Error(`Resource was unable to be reached`);
     }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-    advice.innerText = data.slip.advice;
-    adviceNum.innerText = data.slip.id;
-    advice.classList.add('animate-fadeIn');
-    adviceNum.classList.add('animate-fadeIn');
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
-});
+      
+    const data = await response.json();
+    adviceText = `"${data.slip.advice}"`;
+    adviceId = data.slip.id;
+    advice.classList.add('animate-fadeOut');
+    adviceNum.classList.add('animate-fadeOut');
+  }
+  catch (error){
+    console.error(error);
+  }
+}
+
+dice.addEventListener('click', fetchRandomAdvice);
 
 adviceAnimation.forEach(adviceEle => {
   adviceEle.addEventListener('animationend', e => {
-    adviceEle.classList.remove('animate-fadeIn');
+    if (e.animationName === 'fadeOut') {
+      advice.innerText = adviceText;
+      adviceNum.innerText = adviceId;
+      adviceEle.classList.remove('animate-fadeOut');
+      adviceEle.classList.add('animate-fadeIn');
+    }
   });
-});
-
+})
